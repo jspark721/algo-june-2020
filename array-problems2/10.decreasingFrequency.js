cc8d8a99ce86c125c83d334cc03dd81abc69b7dd
jspark721 <jspark721@gmail.com>
2020-07-13 17:12:51 -0700

    finish problem 10

---
+++ array-problems2/10.decreasingFrequency.js
@@ -0,0 +1,54 @@
+/*Print the elements of an array in the decreasing frequency if 2 numbers have same frequency then print the one which came first.
+
+examples:
+
+input: [2,5,2,8,5,6,8,8]
+ouput: [8,8,8,2,2,5,5,6]
+
+input: [2,5,2,6,-1,9999999,5,8,8,8]
+output: [8,8,8,2,2,5,5,6,-1,9999999]
+
+
+use a hash table, keep track of how many occurences each element occurs, and then print that out to the new array in decreasing frequency 
+
+for example, for input [2,5,2,8,5,6,8,8]
+create hash table {
+  2: 2,
+  5: 2,
+  8: 3,
+  6: 1,
+}
+
+then print out, starting from max key value, and decrement the value
+*/
+
+const decreasingFrequency = (arr) => {
+  let result = [];
+  let hash = {};
+  let sortedHash = {};
+
+  if (arr.length === 0) return result;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!hash[arr[i]]) {
+      hash[arr[i]] = 0;
+    }
+    hash[arr[i]]++;
+  }
+  let keys = Object.keys(hash);
+  keys
+    .sort(function (a, b) {
+      return hash[b] - hash[a];
+    })
+    .forEach(function (num) {
+      while (hash[num] > 0) {
+        result.push(Number(num));
+        hash[num]--;
+      }
+    });
+
+  return result;
+};
+
+console.log(decreasingFrequency([2, 5, 2, 8, 5, 6, 8, 8]));
+console.log(decreasingFrequency([2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8]));
