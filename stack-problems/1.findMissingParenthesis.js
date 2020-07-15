39d50843401cd7e82bd19a92018e93596146614e
Julie Park <jspark721@gmail.com>
2020-07-14 21:48:16 -0700

    solve stack problem 1

--- stack-problems/1.findMissingParenthesis.js
+++ stack-problems/1.findMissingParenthesis.js
@@ -16,30 +16,29 @@ const findMissingParenthesis = (str) => {
 
   //create a stack
   let stack = [];
+  let parenthesis = '';
 
   //loop through the string, if the current character is a starting parenthesis, "(", add it to the stack
   //if the current character is a closing parenthesis, ")", pop it off the stack
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === "(") {
+    if (str[i] === '(') {
       stack.push(str[i]);
     }
-    if (str[i] === ")") {
-      stack.pop();
+    if (str[i] === ')') {
+      parenthesis = stack.pop();
+      if (!parenthesis) {
+        return 'missing opening parenthesis (';
+      }
     }
   }
 
-  console.log(stack);
-  //loop through the stack, and check which is missing
-  for (let i = 0; i < stack.length; i++) {
-    if (stack[i] === "(") {
-      return "missing closing parenthesis )";
-    }
-    if (stack[i] === ")") {
-      return "missing opening parenthesis (";
-    }
+  //if the stack has items in it, then it's missing closing parenthesis
+  if (stack.length > 0) {
+    return 'missing closing parenthesis )';
+  } else {
+    return 'no missing parenthesis';
   }
-  return "no parenthesis missing";
 };
 
-console.log(findMissingParenthesis("- 2 * (3 + 5(sasdfasdfasd)")); // missing )
-console.log(findMissingParenthesis("34 * 4+(21/3 * 5))")); // missing (
+console.log(findMissingParenthesis('- 2 * (3 + 5(sasdfasdfasd)')); // missing )
+console.log(findMissingParenthesis('34 * 4+(21/3 * 5))')); // missing (
