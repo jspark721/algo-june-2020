f03861d2cf951c30be85c33fc9b8fa186aa98bff
Julie Park <jspark721@gmail.com>
2020-07-15 11:57:07 -0700

    pseudocode on stack problem 2

--- stack-problems/2.evaluateExpression.js
+++ stack-problems/2.evaluateExpression.js
@@ -1,4 +1,23 @@
 /*
 Evaluate an expression given only single digits and only 2 operators * and +
 
+
+example:
+
+3 7 9 * +
+given this expression, the output would be 3*7 = 21 + 9 = 30
+
+output result would be 30
+
+
+4 8 3 + * 
+4 + 8 = 12 * 3 = 36
+output would be 36
+
+iterate through the expression string using a for loop
+if it's a digit/number- push it to the stack
+when we see an operator, pop off the stack twice and do the math
+then add it back to the stack
+and when the stack is empty, return the calculation/result
+
 */
