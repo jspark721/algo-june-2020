8eb808db257ffad82dc287ac7762adf9b3e4a0fe
jspark721 <jspark721@gmail.com>
2020-07-16 16:43:48 -0700

    add string problems

---
+++ string-problems/6.longestCommonSubsequence.js
@@ -0,0 +1,6 @@
+/* given two sequences print longest common subsequence
+
+LCS for input Sequences "ABCDGH" and "AEDFHR" is "ADH" of length 3
+LCS for input Sequences "AGGTAB" and "GXTXAYB" is "GTAB" of length 4
+
+*/
