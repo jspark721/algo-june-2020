fc6966d4eda691a8e4ad06bd4e641141b3271380
jspark721 <jspark721@gmail.com>
2020-08-17 17:06:27 -0700

    time and space complexity

--- linked-list-problems/6.deleteNode.js
+++ linked-list-problems/6.deleteNode.js
@@ -12,23 +12,28 @@
 
  */
 
-// const deleteNode = (node) => {
-//   node.val = node.next.val;
-//   node.next = node.next.next;
+// const deleteNode = (head, node) => {
+//   if (head === null) {
+//     return head;
+//   }
+
+//   while (head !== null) {
+//     if (head.val == node) {
+//       head.val = head.next.val;
+//       head.next = head.next.next;
+//     }
+//     head = head.next;
+//   }
+
+//   return head;
 // };
 
-const deleteNode = (head, node) => {
-  if (head === null) {
-    return head;
-  }
+//the above solution doesn't work because the description says we are only given the access to the node, not the head or the list
 
-  while (head !== null) {
-    if (head.val == node) {
-      head.val = head.next.val;
-      head.next = head.next.next;
-    }
-    head = head.next;
-  }
-
-  return head;
+const deleteNode = (node) => {
+  node.val = node.next.val;
+  node.next = node.next.next;
 };
+
+//time complexity: O(1) -- just changing the node value and the node's next pointer
+//space complexity: O(1)
